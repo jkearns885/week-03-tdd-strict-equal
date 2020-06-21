f9afddb10ecb551796c9d91f10569f9583a67980
Julie Kearns <juliekearns@Julies-Air.home>
2020-06-21 15:57:55 -0400

    Strict Equal Function, return true test.

---
+++ tests/app.test.js
@@ -0,0 +1,19 @@
+var describe = require('mocha').describe
+var it = require('mocha').it
+var expect = require('chai').expect
+var strictEqual = require('../app')
+
+describe('app', () => {
+  describe('strictEqual', () => {
+    it('returns true if two inputs are strictly equal to each other & returns false if not', () => {
+      // setup test data
+      var inputOne = 5
+      var inputTwo = 5
+      // call the function with the test data
+      var result = strictEqual(inputOne, inputTwo)
+      // asserts what should be true
+
+      expect(result).to.equal(true)
+    })
+  })
+})
